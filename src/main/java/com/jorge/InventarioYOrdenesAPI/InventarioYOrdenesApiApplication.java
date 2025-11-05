6b2c14e275b444a3fedabe33e59c920f9c16b750
Jorge Alberto Sanchez Nava <jorge.isc.nava@gmail.com>
2025-11-05 16:53:54 -0600

    Creación del proyecto de control de Ordenes y consumo de API externa

---
+++ src/main/java/com/jorge/InventarioYOrdenesAPI/InventarioYOrdenesApiApplication.java
@@ -0,0 +1,13 @@
+package com.jorge.InventarioYOrdenesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioYOrdenesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioYOrdenesApiApplication.class, args);
+	}
+
+}
